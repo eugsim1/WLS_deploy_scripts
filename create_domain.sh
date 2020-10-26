c621396a9592d1c0940e2f54db80d632b8931ac3
eugsim1 <22279788+eugsim1@users.noreply.github.com>
2020-10-26 11:54:11 +0200

    update

--- create_domain.sh
+++ create_domain.sh
@@ -1,6 +1,6 @@
 #### https://github.com/oracle/weblogic-deploy-tooling/blob/master/site/create.md
 #### create a domain_home
-${WDT_HOME}/bin/createDomain.sh \
+createDomain.sh \
 -oracle_home $ORACLE_HOME \
 -java_home $JAVA_HOME \
 -domain_home $DOMAIN_HOME \
@@ -8,4 +8,4 @@ ${WDT_HOME}/bin/createDomain.sh \
 -variable_file <your_properties_file> \
 -model_file   <your_model.yaml_file> \
 -archive_file <your_application_archive> \
--wlst_path    <your_Oracle_home subdirectoryçof_wlst_command>
+-wlst_path    $WLST_PATH

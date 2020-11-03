b86aacf66ff7779f19c98f75246be7961be92df0
eugsim1 <22279788+eugsim1@users.noreply.github.com>
2020-11-03 12:23:01 +0200

    u

--- refresh_from_git.sh
+++ refresh_from_git.sh
@@ -3,3 +3,4 @@ cd /home/oracle && \
 rm -rf /home/oracle/WLS_deploy_scripts && \
 git clone https://github.com/eugsim1/WLS_deploy_scripts.git && \
 chmod u+x /home/oracle/WLS_deploy_scripts/*.sh
+cd /home/oracle/WLS_deploy_scripts
